39e33afccc9ad0604c8e6dc5e6bad38363c52b8d
Pedro C. Costa <52168131+Pedrocereja@users.noreply.github.com>
2022-02-18 12:02:08 -0300

    Update teste.m

--- teste.m
+++ teste.m
@@ -1,9 +1,11 @@
 clear all;
 close all;
 
+
+
 # Dados da simulacao
 numero_pontos = 10000;
-numero_periodos = 6;
+numero_periodos = 8;
 f = 60;
 
 # Condicoes iniciais
@@ -12,22 +14,25 @@ for i = 1:(numero_periodos*numero_pontos)
   H(i) =6e3*sin(2*pi*f*t(i));    
 end
 
-M = JA(H, 0, .1, t);
+#load("Hext.mat");
+
+M = JA(H, 0);
 #uo = 4*pi()*1e-7;
 #B = (M + H)*uo;
 
-#subplot(1,3,1);
-#plot(t,B);
-#xlabel('Tempo[s]');
-#ylabel('B');
 if 1
-  subplot(1,2,1);
+  subplot(1,3,1);
+  plot(t,H);
+  xlabel('Tempo[s]');
+  ylabel('H');
+
+  subplot(1,3,2);
   plot(t,M);
   xlabel('Tempo[s]');
   ylabel('M');
 
-  subplot(1,2,2);
-  plot(H,M);
+  subplot(1,3,3);
+  plot(H,M/1.6e6);
   xlabel('H[A/m]');
   ylabel('M');
 endif
