a71cc96423e659f2bd16293f1117551716d96825
Pedro C. Costa <52168131+Pedrocereja@users.noreply.github.com>
2022-02-22 15:59:02 -0300

    Metodo de calculo alterado
    A equação diferencial foi ajustada para o modelo de Cancun. Além disso, os parâmetros agora são passados como variáveis.

--- JA.m
+++ JA.m
@@ -1,27 +1,29 @@
-function M = JA(H, Mo)
+function M = JA(H, Mo, Parametros)
   # A funcao retorna o vetor da magnetizacao, M, a partir dos argumentos:
   # - H:   vetor do campo magnetico externo aplicado;
   # - Mo: condicao inicial para a magnetizacao.
+  # - Parametros: parâmetros do material no formato [k, c, a, Ms, alpha]
   
   numero_pontos = length(H); ### o vetor M possuira o mesmo numero de pontos que H
   
   ## Constantes e condicao inicial
-  k=400;
-  c=0.27;
-  a=1100;
+  p = Parametros;
+  k=p(1);
+  c=p(2);
+  a=p(3);
+  Ms=p(4);
+  alpha=p(5);
   uo = 4*pi()*1e-7;
-  Ms=1.6e6;
-  alpha=1.6e-3;
   M(1) = Mo;
 
   for i = 1:numero_pontos-1
     ## Equacoes auxiliares
     He = H(i) + alpha*M(i);
     if abs(He/a) <= 0.1 ### para valores pequenos de He e preciso expandir a formulacao em series de Taylor, do contrario o resultdo vai para infinito
-      Man(i) = Ms * (He/(3*a) - ((He/a)^ 2)/45);
+      Man = Ms * (He/(3*a));
       dMan_dHe = Ms * (1/(3*a));
     else
-      Man(i) = Ms*(coth(He/a) - a/He);
+      Man = Ms*(coth(He/a) - a/He);
       dMan_dHe = (Ms/a)*(1-(coth(He/a))^2+(a/He)^2);
     end
     ### delta
@@ -33,21 +35,12 @@ function M = JA(H, Mo)
     end
     
     ## Equacao derivativa
-    dM_dH(i) = 1/(1+c) * (Man(i)-M(i))/(delta*k/uo - alpha*(Man(i)-M(i)));
-    dM_dH(i) += c/(1+c) * dMan_dHe;
+    #dM_dH = 1/(1+c) * (Man(i)-M(i))/(delta*k/uo - alpha*(Man(i)-M(i)));
+    #dM_dH += c/(1+c) * dMan_dHe;
+    dM_dH = c*dMan_dHe + (1-c)*(Man - M(i)) / (k*delta*(1-c)-alpha*(Man-M(i)));
+    dM_dH /= (1- alpha*c);
     
     ## Método de euler
-    M(i+1) = M(i) + dH*dM_dH(i);
+    M(i+1) = M(i) + dH*dM_dH;
   end
-  
-  ## Debug
-  debug_mode = 0;
-  if debug_mode == 1
-    ### curva anisteretica
-    Man(numero_pontos) = Ms*(coth(He/a) - a/He);
-    subplot(1, 2, 1)
-    plot(H,  Man);
-    xlabel('H');
-    ylabel('Man');
-  endif
 endfunction
